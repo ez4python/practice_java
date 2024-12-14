36960e56caa7a7f4be1ce63a0a9b367e80219d1c
Akbarali Salohiddinov <akbaralisalohiddinov808@gmail.com>
2024-12-14 21:23:28 +0500

    Class Main change to public

--- src/testprogram/Main.java
+++ src/testprogram/Main.java
@@ -18,14 +18,14 @@ class Dog extends Animal {
     }
 }
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Animal myAnimal = new Animal();
         Animal myPig = new Pig();
-        Animal myDog = new Dog();  
+        Animal myDog = new Dog();
         myAnimal.animalSound();
         myPig.animalSound();
         myDog.animalSound();
     }
-    
+
 }
